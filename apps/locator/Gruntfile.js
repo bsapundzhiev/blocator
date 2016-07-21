2f164b3d931d09de0893d7fca382d8f05073bffc
Borislav Sapundzhiev <BSapundjiev@gmail.com>
2016-07-22 00:58:45 +0300

    added project.json

---
+++ apps/locator/Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: '../../scripts',
+                    src: ['utils/wsclient.js', 'controllers/map.js', 'controllers/open-street-map-leaflet.js'],
+                    dest: 'www/js'
+                }]
+            },
+	    libs: {
+                files: [{
+                    expand: true,
+                    cwd: '../../',
+                    src: ['libs/*/*'],
+                    dest: 'www/'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['copy']);
+};
